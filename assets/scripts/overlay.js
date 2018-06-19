87fad9fc37981875cf240f3250ea91cb1a5d638e
kevin <kevinpiersoul.com>
2018-06-19 17:22:24 +0200

    fixed sound issue while chaining

--- assets/scripts/overlay.js
+++ assets/scripts/overlay.js
@@ -6,9 +6,7 @@ let DLOverlay = function(author, voice){
 	
 	self.author = author;
 	self.voice = voice;
-
 	self.switcheroo = 1;
-	
 	self.votesHistory = []
 	self.messageHistory = [];
 	
@@ -38,93 +36,88 @@ let DLOverlay = function(author, voice){
 	}
 	
 	//public methods
-	let initialize = function(author, voice){
-		self.author = author;
-		self.voice = voice;
-	}
-	
 	let start = function() {
 		let playing = false;
 		let loopChecking = setInterval(function(){
 			if( !playing ){
 				switch( self.switcheroo ){
-				case 1: 
-					getContent( function(response){
-						
-						let voteList = response.result.votes;
-						let newVotes = voteList.slice(self.votesHistory.length, voteList.length);
-						
-						let recursiveSpeakAndShow = function(i){
+					case 1: 
+						//mention the upvoters
+						getContent( function(response){
 							
-							let voter = newVotes[i]['voter'];
+							let voteList = response.result.votes;
+							let newVotes = voteList.slice(self.votesHistory.length, voteList.length);
 							
-							let percent = newVotes[i]['percent'];
-							percent = percent / 100;
-							
-							let message = 'voted for '+percent+ ' percent';
-							
-							
-							speakAndShow(voter, message + '<br><img src="assets/images/vote.gif">', voter + " " +message, function(){ }, function(){ 
-								let sound = new Audio('assets/audio/vote.mp3');
-								sound.play();
+							let recursiveSpeakAndShow = function(i){
 								
-								if( i < newVotes.length-1 ){
-									let index = i+1;
-									recursiveSpeakAndShow( index );
-								}else{
-									playing = false;
-								}
-							});
+								let voter = newVotes[i]['voter'];
+								
+								let percent = newVotes[i]['percent'];
+								percent = percent / 100;
+								
+								let message = 'voted for '+percent+ ' percent';
+								
+								
+								speakAndShow(voter, message + '<br><img src="assets/images/vote.gif">', voter + " " +message, function(){ }, function(){ 
+									
+									if( i < newVotes.length-1 ){
+										let index = i+1;
+										recursiveSpeakAndShow( index );
+									}else{
+										playing = false;
+									}
+								});
+								
+							}
 							
-						}
-						
-						if( newVotes.length > 0 ){
-							playing = true;
-							recursiveSpeakAndShow( 0 );
-						}
-						self.votesHistory = voteList;
-					});
-					self.switcheroo = 2;
+							if( newVotes.length > 0 ){
+								playing = true;
+								recursiveSpeakAndShow( 0 );
+							}
+							self.votesHistory = voteList;
+						});
+						self.switcheroo = 2;
 					break;
-				case 2: 
-					getContentReplies( function(response){
-						
-						let replyList = response.result.discussions;
-						let newReplies = replyList.slice(self.messageHistory.length, replyList.length);
-						
-						let recursiveSpeakAndShow = function(i){
-							let commenter = newReplies[i].author;
-							let message = newReplies[i].body;
+					//text to speech text
+					case 2: 
+						getContentReplies( function(response){
 							
+							let replyList = response.result.discussions;
+							let newReplies = replyList.slice(self.messageHistory.length, replyList.length);
 							
-							for( let i = 0; i < self.votesHistory.length; i++ ){
-								if(commenter === self.votesHistory[i]['voter'] ){
+							let recursiveSpeakAndShow = function(i){
+								let commenter = newReplies[i].author;
+								let message = newReplies[i].body;
+								
+								
+								for( let i = 0; i < self.votesHistory.length; i++ ){
+									if(commenter === self.votesHistory[i]['voter'] ){
+										
+										speakAndShow(commenter, message, message, function(){}, function(){
+											if( i < newReplies.length ){
+												let index = i+1;
+												recursiveSpeakAndShow(index);
+											}else{
+												playing = false;
+											}
+										});
+										
+										break;
+									}
 									
-									speakAndShow(commenter, message, message, function(){}, function(){
-										if( i < newReplies.length ){
-											let index = i+1;
-											recursiveSpeakAndShow(index);
-										}else{
-											playing = false;
-										}
-									});
-									
-									break;
 								}
 								
 							}
 							
-						}
+							if( newReplies.length > 0 ){
+								playing = true;
+								recursiveSpeakAndShow(0);
+							}
+							
+							self.messageHistory = replyList;
+						});
 						
-						if( newReplies.length > 0 ){
-							playing = true;
-							recursiveSpeakAndShow(0);
-						}
-						
-						self.messageHistory = replyList;
-					});
-					
-					self.switcheroo = 1;
+						self.switcheroo = 3;
 					break;
 				}
 			}
@@ -153,11 +146,17 @@ let DLOverlay = function(author, voice){
 		//select messagebox and append text
 		let messageBox = document.querySelector(".message");
 		messageBox.innerHTML = writtenText;
-		
+		//fade the message in
 		$("#msgWrapper").fadeToggle( "slow", function(){
 			//play the message when fully visible
 			responsiveVoice.speak(spokenText, self.voice, {onstart: fadeInCallBack, onend: function(){
-				$("#msgWrapper").fadeToggle( "slow" ).delay( 1000 ).queue( fadeOutCallBack() );
+				//play the audio after the message has been spoken
+				let sound = new Audio('assets/audio/vote.mp3');
+				sound.play();
+				//fade out when the audio is done and call the callback when fully completed
+				sound.addEventListener("ended", function(){
+					$("#msgWrapper").fadeToggle( "slow", fadeOutCallBack );
+				})
 			}});
 		})
 	}
