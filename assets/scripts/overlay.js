0321b09f5a44102a82cff68ef16689addc51b0df
kevin <kevinpiersoul.com>
2018-07-05 00:59:26 +0200

    1)added method to allow custom functions to the cycle 2) made the start cycle scalable 3)option to disable build in upvote and reply handler

--- assets/scripts/overlay.js
+++ assets/scripts/overlay.js
@@ -6,21 +6,32 @@
  * @params {string} author The steemit account of the streamer
  * @params {string} permlink The last uri part of the streamer's steemit post, this resembles the last uri part of the DLive stream as well
  * @params {string} voice Specify which voice to use for text to voice. Go to: https://responsivevoice.org/ for a full list of voices
+ * @params {boolean} enableUpvoteHandler set to false to disable the build in upvote handler
+ * @params {boolean} enableReplyHandler set to false to disable the build in reply handler
  */
-
-let DLOverlay = function(author, permlink, voice){
+let DLOverlay = function(author, permlink, voice, enableUpvoteHandler = true, enableReplyHandler = true){
             
     let self = this;
     
     self.author = author;
     self.permlink = permlink;
     self.voice = voice;
-    self.switcheroo = 1;
     self.votesHistory = [];
     self.replyHistory = [];
     self.playing = false;
     
     self.speakandshowQueue = [];
+    
+    self.actionCycleList = [];
+    //add the build in upvote handler to the cycle if allowed
+    if(enableUpvoteHandler){
+        handleUpvotes();
+    }
+     //add the build in reply handler to the cycle if allowed
+    if( enableReplyHandler ){
+        handleReplies();
+    }
+    
     
     // private methods
     
@@ -30,7 +41,6 @@ let DLOverlay = function(author, permlink, voice){
      * @params {array} params the parameters we send to the api's specific method
      * @params {function}callback if succesfull trigger this function
      */
-    
     function apiCall(method, params, callback){
         let request = {
             method: method,
@@ -59,7 +69,6 @@ let DLOverlay = function(author, permlink, voice){
     * get the next item in the playqueue, remove it from the playqueue and trigger the speak and show method with the data
     * if after completion the playqueue is not empty recursevely trigger the method again
     */
-    
     function playQueue(){
         let sASPars = self.speakandshowQueue.shift();
         speakAndShow(sASPars.author, sASPars.writtenText, sASPars.spokenText, sASPars.fadeInCallBack, function(){
@@ -79,9 +88,7 @@ let DLOverlay = function(author, permlink, voice){
      * @params {function} soundStartsCallBack trigger a function when the audio starts
      * @params {function} soundEndsCallBack trigger a function when audio ends
      */
-    
     let speakAndShow = function( author, writtenText, spokenText, soundStartsCallBack, soundEndsCallBack ){
-        
         //let the system know something is playing
         self.playing = true;
         //select authorBox and append author
@@ -109,7 +116,71 @@ let DLOverlay = function(author, permlink, voice){
             }});
         })
     }
+    
+    /**
+     * Add the build in upvote handler to the cycle Queue
+     */
+    function handleUpvotes(){
+        addCyclingAction( function(){
+            getContent( function(response){
+                let voteList = response.result.votes;
+                
+                //loop all object in response
+                for( let o of voteList ){
+                    //check if we already triggered a show and speak with this upvote
+                    let found = self.votesHistory.find( val => val == o.voter )?true:false;
+                    
+                    //if value doesn't exist this speak and show has not yet been displayed, add it to the handle list and to the play que  
+                    if( found === false ){
+                        //add it to the handled list
+                        self.votesHistory.push( o.voter );
+                        //get the voter
+                        let voter = o.voter;
+                        //get the percentage for which they vote
+                        let percent = o.percent;
+                        percent = percent / 100;
+                        //add the percent to a custom message
+                        let message = `voted for ${percent} percent`;
+                        
+                        //add it to the play que
+                        addSpeakAndShow(voter, `${message}<br><img src="assets/images/vote.gif" style="width: 350px; height:200px;">`, `${voter} ${message}`, function(){ }, function(){});
+                    }
+                }
+                
+            });
+        });
+    }
+    
+    /**
+     * Add the build in reply handler to the cycle Queue
+     */
+    function handleReplies(){
+        addCyclingAction( function(){
+             getContentReplies( function(response){
+                
+                let replyList = response.result.discussions;
+                
+                //loop all object in response
+                for( let o of replyList ){
+                    //if the message has been played or the author did not upvote the stream put found to true to not trigger the speak and show
+                    let found = self.replyHistory.find( val => val == o.author ) || !self.votesHistory.find( val => val == o.author )?true:false;
+                    
+                    //if value doesn't exist and the author upvoted  add it to the handle list and to the play que  
+                    if( found === false ){
+                        //add it to the handled list
+                        self.replyHistory.push( o.author );
+                        //get the author
+                        let commenter = o.author;
+                        //get the message
+                        let message = o.body;
 
+                        //add it to the play que
+                        addSpeakAndShow(commenter, message, message, function(){}, function(){});
+                    }
+                }
+            });
+        })
+    }
     
     //public methods
     
@@ -118,72 +189,17 @@ let DLOverlay = function(author, permlink, voice){
      * Check if something is playing and if not if there is something in que that needs to be played
      * check for new data to add to the playing queue
      */
-    let start = function() {
+    function start() {
+        let index = 0;
+        
         let loopChecking = setInterval( function(){
             if( !self.playing && self.speakandshowQueue.length > 0 ){
                 playQueue();
             }
-
-            switch( self.switcheroo ){
-                case 1: 
-                    //mention the upvoters
-                    getContent( function(response){
-                        
-                        let voteList = response.result.votes;
-                        
-                        //loop all object in response
-                        for( let o of voteList ){
-                            //check if we already triggered a show and speak with this upvote
-                            let found = self.votesHistory.find( val => val == o.voter )?true:false;
-                            
-                            //if value doesn't exist this speak and show has not yet been displayed, add it to the handle list and to the play que  
-                            if( found === false ){
-                                //add it to the handled list
-                                self.votesHistory.push( o.voter );
-                                //get the voter
-                                let voter = o.voter;
-                                //get the percentage for which they vote
-                                let percent = o.percent;
-                                percent = percent / 100;
-                                //add the percent to a custom message
-                                let message = `voted for ${percent} percent`;
-                                
-                                //add it to the play que
-                                addSpeakAndShow(voter, `${message}<br><img src="assets/images/vote.gif" style="width: 350px; height:200px;">`, `${voter} ${message}`, function(){ }, function(){});
-                            }
-                        }
-                    });
-                    self.switcheroo = 2;
-                break;
-                //text to speech text
-                case 2: 
-                    getContentReplies( function(response){
-                        
-                        let replyList = response.result.discussions;
-                        
-                        //loop all object in response
-                        for( let o of replyList ){
-                            //if the message has been played or the author did not upvote the stream put found to true to not trigger the speak and show
-                            let found = self.replyHistory.find( val => val == o.author ) || !self.votesHistory.find( val => val == o.author )?true:false;
-                            
-                            //if value doesn't exist and the author upvoted  add it to the handle list and to the play que  
-                            if( found === false ){
-                                //add it to the handled list
-                                self.replyHistory.push( o.author );
-                                //get the author
-                                let commenter = o.author;
-                                //get the message
-                                let message = o.body;
-
-                                //add it to the play que
-                                addSpeakAndShow(commenter, message, message, function(){}, function(){});
-                            }
-                        }
-                    });
-                    
-                    self.switcheroo = 1;
-                break;
-            }
+            console.log(  self.actionCycleList.length );
+            self.actionCycleList[index]();
+            //if the index key + 1 is smaller than the amount of actions add one, otherwise put it back to 0 to start the cycle over again
+            index = index+1 < self.actionCycleList.length?index+1:0;
         }, 2500)
         
     }
@@ -193,7 +209,7 @@ let DLOverlay = function(author, permlink, voice){
      * @params {function} callback A function to execute when we've succesfully gotten a response from the api, use a response parameter to handle the server's response in the function
      */
      
-    let getContent = function( callback ){
+    function getContent( callback ){
         apiCall('tags_api.get_active_votes', {
             "author": self.author, 
             "permlink":self.permlink
@@ -204,15 +220,21 @@ let DLOverlay = function(author, permlink, voice){
      * make a call to steemit's api to get the replies of the article that is linked with the livestream
      * @params {function} callback A function to execute when we've succesfully gotten a response from the api, use a response parameter to handle the server's response in the function
      */
-    
-    let getContentReplies = function( callback ){
+    function getContentReplies( callback ){
         apiCall('tags_api.get_content_replies',  {
             "author":self.author,
             "permlink":self.permlink
         }, callback)
     }
     
-    //add commands to the speakandshowQueue
+    /**
+     * Add a new method that has to be added to the cycle when start is called
+     * @params {function} method A function that will be called every x amount of time 
+     */
+    function addCyclingAction( method ){
+        self.actionCycleList.push( method )
+    }
+    
     /**
      * Add an object that has properties with values that resemble the parameters of the speakAndShow method to the speakandshowQueue array}
      * Has to be added as an object not an array so we can use the exact same keys instead of index numbers
@@ -236,8 +258,8 @@ let DLOverlay = function(author, permlink, voice){
     /**
      * return an object with all the global properties and methods
      */
-    
     return{
+        addCyclingAction:           addCyclingAction,
         addSpeakAndShow:            addSpeakAndShow,
         getContent:                 getContent,
         getContentReplies:          getContentReplies,
